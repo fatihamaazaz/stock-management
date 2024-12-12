14388def3ae489c8dcbf22ab3afbd9eb258dc30f
fatiha <fatihamaazaz@gmail.com>
2024-12-12 16:30:03 +0100

    ci: adjust containers links

--- devops/smee/commands.sh
+++ devops/smee/commands.sh
@@ -2,4 +2,4 @@
 
 npm install --global smee-client
 
-smee --url https://smee.io/9v46c6cbSy3TKZzV --path /github-webhook/ --port 3030 --target http://jenkins:8080/github-webhook/
+smee --url https://smee.io/9v46c6cbSy3TKZzV --path /github-webhook/ --port 3030 --target http://devops-jenkins-1:8080/github-webhook/
